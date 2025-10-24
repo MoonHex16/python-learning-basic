cc993c68aea44043cd1f7a49bc6354ec3f78b239
RM <mac@macs-MacBook-Pro.local>
2025-10-24 23:05:47 +0530

    second commit

---
+++ october/day_02.py
@@ -0,0 +1,39 @@
+name = "moon"
+age = 24
+goal = "become a developer"
+
+print(name)
+print(age)
+print(goal)
+
+# add
+x = 10
+y = 20
+
+sum = (x + y)
+
+print(sum)
+
+# subs
+x = 10
+y = 20
+
+sum = (x - y)
+
+print(sum)
+
+# multy
+x = 10
+y = 20
+
+sum = (x * y)
+
+print(sum)
+
+# divide
+x = 10
+y = 20
+
+sum = (x /y)
+
+print(sum)
---
+++ october/day_06.py
@@ -0,0 +1,25 @@
+# task 1
+
+for i in range (1, 11):
+    print(i)
+
+# task 2
+
+for i in range (5, 16):
+    print(i)
+
+
+for i in range (1, 21):
+    if i%2 != 0:
+        print(i)
+    
+
+for i in range(10,0,-1):
+    print(i)
+
+n = int(input("chose any one num: "))
+i = 1
+while i <= 10:
+    print(i * n)
+    i += 1
+
