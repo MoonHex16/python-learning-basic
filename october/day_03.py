d10bcab51f705b527ea9577a82b98a4111cd7d7c
RM <mac@macs-MacBook-Pro.local>
2025-10-24 22:51:05 +0530

    first commit

---
+++ october/day_03.py
@@ -0,0 +1,12 @@
+# Task_1
+# name = input("what is your name :")
+# age = input("what is your age :")
+# city = input("what is your city :")
+
+# print("Hey",name, "you are", age, "and live in", city)
+
+# Task_2
+num_1 = int(input("give your first no. : "))
+num_2 = int(input("give your second no. : "))
+num = (num_1 + num_2)
+print(num)
